d5b26fdcc929b5f203d710cf6447732e104aea76
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-08-16 09:04:57 +0200

    pop

--- list.py
+++ list.py
@@ -31,4 +31,72 @@ print(2*lc)
 listA = [1,2,3,4,5]
 listA.insert(0,11)
 print(listA)
+listA.insert(2,555)
+print(listA)
 print('hello listt')
+
+# reverse
+listA.reverse()
+print(listA)
+listA.extend(lb)
+print(listA)
+
+# sort
+listA.sort()
+print(listA)
+
+""" # Implement the Python function myreduce(a) that returns
+the sum of the elements of the list a. """
+def sum_list(s):
+    sum = 0
+    for x in s:
+        sum += x
+    return sum
+
+a = [10,20,30,40]
+print(sum_list(a))
+print(sum(a))
+
+
+# capitalized first letter
+def capitalized_all(s):
+    result = []
+    for x in s:
+        result.append(x.capitalize())
+    return result
+
+a = ["hello", "how", "are", "you"]
+print(capitalized_all(a))
+
+
+# only upper case
+def only_upper(s):
+    result = []
+    for x in s:
+        if x.isupper():
+            result.append(x)
+    return result
+
+a = ["How", 'how', 'are', 'You?']
+print(capitalized_all(a))
+print("all character are capitalized: ",only_upper(a))
+
+
+lista = [1,2,3]
+x = lista.pop(0)
+print(x)
+print(lista)
+
+
+
+
+
+""" Implement the Python function mymap(a) that returns a list
+containing the elements of the list a each multiplied by 2. """
+
+
+
+
+""" Implement the Python function myfilter(a) that returns a
+list containing the odd elements of the list a each multiplied
+by 2. """
