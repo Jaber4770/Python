2ed56f0cbb8ccc5f4287b774e04e8c6260150c77
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-08-18 15:27:20 +0200

    DSA - insertion sort

--- set.py
+++ set.py
@@ -4,3 +4,30 @@ bSet = {3,4,5,6,7,8,9}
 numbers =[2,3,4,5]
 nSet = set(numbers)
 print(nSet)
+
+
+emptySet = set()
+print(emptySet)
+EmptySet = {}
+print(EmptySet)
+
+print("Length of a set: ",len(aSet))
+if 45 in aSet:
+    print("the element", 45, "is contained in the set")
+    
+
+
+ca = {'red',"white"}
+it = {"red","white","green"}
+fr = {"red","white","bluee"}
+
+if ca.issubset(it):
+    print("subset")
+
+if it == fr:
+    print("the same color")
+    
+if ca != fr:
+    print("not the same colors")
+
+
