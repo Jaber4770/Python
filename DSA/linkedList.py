c63a17d3be91f3668c6b2d613073ed1bfc18dd27
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-08-30 20:59:54 +0200

    linked list

---
+++ DSA/linkedList.py
@@ -0,0 +1,13 @@
+""" 
+A doubly linked list is implemented as a dictionary of dictionaries like, for example, the following: 
+
+dll = { 'head': {'prev': None, 'next': 12},
+12: {'prev': 'head', 'next': 24},
+24: {'prev': 12, 'next': 37},
+37: {'prev': 24, 'next': 14},
+14: {'prev': 37, 'next': 3},
+3: {'prev': 14, 'next': 'tail'},
+'tail': {'prev': 3, 'next': None}}
+Implement the Python function delete_first(dll) that removes the node 
+closer to the head and returns the doubly linked list modified.
+"""
