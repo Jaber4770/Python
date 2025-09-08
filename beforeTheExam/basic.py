01c0c14fdd207e09db9e9cd837de559c721610a4
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-09-08 21:16:20 +0200

    variable

--- beforeTheExam/basic.py
+++ beforeTheExam/basic.py
@@ -12,3 +12,31 @@ y = 'hello exam!'
 
 # single line comment
 """ multitle comment """
+
+#variable
+a,b,c = 'a','b','c'
+print(a,b,c)
+
+aa = 'hello exam, i am comming'
+print(aa)
+
+#list
+fruits = ["banana", "apple", "cherry"]
+print(fruits)
+
+p = 'python is awesome'
+print(p)
+
+pp = 'python'
+yy = 'is'
+aaa = 'awesome'
+print( pp + " " + yy +" "+ aaa)
+
+
+#global variable
+globalVariable = 'its a global variable'
+def func(globalVariable):
+    print(globalVariable)
+    
+func(globalVariable)
+
